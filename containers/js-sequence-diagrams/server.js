f02c31b9445f527616bb235b3b7ee77030c93705
Willem van Heemstra <willem@vanheemstrasystems.com>
2021-05-28 16:44:05 +0200

    Update server.js

--- containers/js-sequence-diagrams/server.js
+++ containers/js-sequence-diagrams/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,8 +9,13 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World\r\n');
+  // res.send('Hello World\r\n');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, HOST);
